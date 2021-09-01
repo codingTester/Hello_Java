230e5d402d38bf1272eaf95ac1e00ba41a365f44
Yuji Henry <yuji.henry@yahoo.com>
2021-09-01 15:12:19 -0700

    This is typo update

--- Hello.java
+++ Hello.java
@@ -2,6 +2,6 @@ public class Hello {
     public static void main(String[] args) {
     System.out.println("Hello, world!");
     System.out.println("\n");
-    System.out.println("From another world!);
+    System.out.println("From another world!");
     }
 }
